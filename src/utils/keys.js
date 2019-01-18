ca0bbe0f889a48693a34c6c57b3c1b9e92568f09
jay.chen <1162512690@qq.com>
2019-01-18 14:33:06 +0800

    remove keys

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,4 +1,4 @@
 //高德key
-export const gaodeMapKey = '高德地图JSAPI公钥' 
+export const gaodeMapKey = '你的高德地图JSAPI公钥'
 //百度地图key
-export const baiduMapKey = '百度地图JSAPI公钥' 
+export const baiduMapKey = '你的百度地图JSAPI公钥'
