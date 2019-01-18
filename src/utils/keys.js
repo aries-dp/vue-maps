da31c81ff99aab97c90f6507f5bee2b52d7b9ffa
jay.chen <1162512690@qq.com>
2019-01-18 14:04:37 +0800

    去掉公钥

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,4 +1,4 @@
 //高德key
-export const gaodeMapKey = '高德地图JSAPI公钥' //eddb58fd0bad15a7ca0a3591baa60f8c
+export const gaodeMapKey = '高德地图JSAPI公钥' 
 //百度地图key
-export const baiduMapKey = '百度地图JSAPI公钥' //ckNbPK3XzGd3pwjzpbMMv3YRGdIOR8Hy
+export const baiduMapKey = '百度地图JSAPI公钥' 
